dbd97e7f83f5f8670d9297bfbcd47551ecbeb800
sameer garg <sameer.garg@which.co.uk>
2014-10-07 12:01:16 +0100

    supermarket kata refactored

--- scala-katas/src/main/scala/katas/supermarket/Checkout.scala
+++ scala-katas/src/main/scala/katas/supermarket/Checkout.scala
@@ -12,7 +12,10 @@ class BuyNGetMFreeOffer(n: Int, m: Int) extends Offer {
   override def offerPrice(shoppingBasket: List[String]): Int = {
     val size = shoppingBasket.size
     val prices = shoppingBasket.flatMap(itemPrice.get(_)).sortWith((x, y) => x > y)
-    prices.foldRight(0)(_ + _) - prices.takeRight(size/n).foldRight(0)(_ + _)
+    val totalPrice = prices.foldRight(0)(_ + _)
+    val offerItemsPrice = prices.takeRight(size / n).foldRight(0)(_ + _)
+
+    totalPrice - offerItemsPrice
   }
 }
 
@@ -25,20 +28,11 @@ class Checkout {
   }
 
   def oneForOneOffer(shoppingCart: ShoppingCart): Int = {
-    val offerItemPrices = shoppingCart.filter(name => name == "Apple" || name == "Banana").map(name => if (name == "Apple") 60 else if (name == "Banana") 100 else 0)
-
-    val sortedPrices = offerItemPrices.sortWith((x, y) => x > y)
-
-    sortedPrices.take((sortedPrices.size + 1) % 2).foldRight(0)(_ + _)
-
-    //new BuyNGetMFreeOffer(2,1).offerPrice(shoppingCart.filter(name => name == "Apple" || name == "Banana"))
-    //(apples/2 * 60 + apples%2 * 60) + (bananas/2 * 100 + bananas%2 * 100)
+    new BuyNGetMFreeOffer(2,1).offerPrice(shoppingCart.filter(name => name == "Apple" || name == "Banana"))
   }
 
   private def threeForTwoOffer(shoppingCart: ShoppingCart): Int = {
-    val count = shoppingCart.count(_ == "Orange")
-    count * 2 / 3 * 25 + count % 3 * 25
-    //new BuyNGetMFreeOffer(3,2).offerPrice(shoppingCart.filter(_ == "Orange"))
+    new BuyNGetMFreeOffer(3,2).offerPrice(shoppingCart.filter(_ == "Orange"))
   }
 
 
