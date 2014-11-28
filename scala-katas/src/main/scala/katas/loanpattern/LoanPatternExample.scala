6d16c31fad1fc16f61189621584d30da881a902a
sameer garg <garg.sameer@gmail.com>
2014-11-28 13:56:48 +0000

    Loan pattern

---
+++ scala-katas/src/main/scala/katas/loanpattern/LoanPatternExample.scala
@@ -0,0 +1,20 @@
+package katas.loanpattern
+
+
+object LoanPattern extends App {
+
+  case class Resource(id: Int)
+
+  def resourceLender[T](f: Resource => T) = {
+    val resource = Resource(1)
+    f(resource)
+  }
+
+  val lendee = {resource: Resource =>
+    println(resource.id)
+  }
+
+    resourceLender(lendee)
+}
+
+
