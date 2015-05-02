b36070f5e1da4d868c4568deb6fed728c35a56e7
sameer garg <garg.sameer@gmail.com>
2015-05-02 18:51:17 +0100

    Week 2 PORP - Final submission

--- PORPAssignments/calculator/src/main/scala/calculator/Polynomial.scala
+++ PORPAssignments/calculator/src/main/scala/calculator/Polynomial.scala
@@ -8,15 +8,17 @@ object Polynomial {
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
                        c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    Signal(
+    Signal({
       delta() match {
-        case d if (d < 0) => Set()
-        case d if (d == 0) => Set((-1 * b()) / (2 * a()))
-        case d => Set(
-          (-1 * b() + Math.sqrt(d)) / (2 * a()),
-          (-1 * b() - Math.sqrt(d)) / (2 * a())
+        case 0 => Set((-1 * b()) / (2 * a()))
+        case d if (d > 0) => Set(
+          (-1 * b() - Math.pow(delta(), 0.5)) / (2 * a()),
+          (-1 * b() + Math.pow(delta(), 0.5)) / (2 * a())
         )
+        case _ => {
+          Set()
+        }
       }
-    )
+    })
   }
 }
