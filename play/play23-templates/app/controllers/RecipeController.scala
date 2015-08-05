2ccfa898bd207d40daae8fc2627cb4ff7cd2042c
sameergarg <garg.sameer@gmail.com>
2015-08-05 14:38:57 +0100

    Included swagger

--- play/play23-templates/app/controllers/RecipeController.scala
+++ play/play23-templates/app/controllers/RecipeController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import com.wordnik.swagger.annotations._
 import models.Recipe
 import play.api.libs.json.Json
 import play.api.mvc.{Controller, Action}
@@ -7,6 +8,7 @@ import play.api.mvc.{Controller, Action}
 /**
  *
  */
+@Api(value = "/recipes", description = "Recipes operations")
 object RecipeController extends Controller {
 
   val recipes = List(
@@ -14,6 +16,16 @@ object RecipeController extends Controller {
     Recipe(name = "cake", ingredients = List("Powder", "Sugar", "Flavour"))
   )
 
+  @ApiOperation(
+    value = "Searches the recipes",
+    notes = "Search the recipes with ingredients matching the query parameter `keyword`",
+    nickname = "recipe",
+    httpMethod = "GET")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "keyword", value = "ingredient item", required = false,
+      dataType = "application/json", paramType = "queryParam")))
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Matching search results")))
   def getRecipes(keyword: Option[String]) = Action {
     val matchingRecipes: List[Recipe] = keyword.collect {
       case word:String => recipes.filter(_.ingredients.mkString.contains(word))
