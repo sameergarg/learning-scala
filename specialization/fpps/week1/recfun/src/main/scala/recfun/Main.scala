d270b151d9c6ab71ffff088c0f636051a6d94f44
sameergarg <garg.sameer@gmail.com>
2016-05-27 12:54:50 +0100

    Example assignment

--- specialization/fpps/week1/recfun/src/main/scala/recfun/Main.scala
+++ specialization/fpps/week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,41 +13,44 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || r == c)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
 
-    def loop(acc: Int, col: Int, row: Int): Int = {
-        if(col == 0 || row == col)
-          acc + 1
-        else
-          loop(acc, col - 1, row -1) + loop(acc, col, row - 1)
-      }
-
-      loop(0, c, r)
-    }
-  
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = {
+    * Exercise 2
+    */
 
-      def loop(acc: Int, rem: List[Char]): Boolean = {
-        if (acc < 0)
-          false
-        else rem match {
-          case Nil => acc == 0
-          case '('::tail => loop(acc + 1, tail)
-          case ')'::tail => loop(acc - 1, tail)
-          case head::tail => loop(acc, tail)
-        }
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def loop(acc: Int, rem: List[Char]): Boolean = {
+      if (acc < 0)
+        false
+      else rem match {
+        case Nil => acc == 0
+        case '(' :: tail => loop(acc + 1, tail)
+        case ')' :: tail => loop(acc - 1, tail)
+        case head :: tail => loop(acc, tail)
       }
-
-      loop(0, chars)
     }
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+
+    loop(0, chars)
   }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0)
+      1
+    else if (money < 0 || coins.isEmpty)
+      0
+    else
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+
+  }
+}
