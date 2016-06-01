5a2a3197610e750569bb85332cbefd036f6427fd
sameergarg <garg.sameer@gmail.com>
2016-06-01 17:15:03 +0100

    Week 2 -> functional sets

---
+++ specialization/fpps/week2/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -0,0 +1,144 @@
+package funsets
+
+import org.scalatest.FunSuite
+
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * This class is a test suite for the methods in object FunSets. To run
+ * the test suite, you can either:
+ *  - run the "test" command in the SBT console
+ *  - right-click the file in eclipse and chose "Run As" - "JUnit Test"
+ */
+@RunWith(classOf[JUnitRunner])
+class FunSetSuite extends FunSuite {
+
+  /**
+   * Link to the scaladoc - very clear and detailed tutorial of FunSuite
+   *
+   * http://doc.scalatest.org/1.9.1/index.html#org.scalatest.FunSuite
+   *
+   * Operators
+   *  - test
+   *  - ignore
+   *  - pending
+   */
+
+  /**
+   * Tests are written using the "test" operator and the "assert" method.
+   */
+  // test("string take") {
+  //   val message = "hello, world"
+  //   assert(message.take(5) == "hello")
+  // }
+
+  /**
+   * For ScalaTest tests, there exists a special equality operator "===" that
+   * can be used inside "assert". If the assertion fails, the two values will
+   * be printed in the error message. Otherwise, when using "==", the test
+   * error message will only say "assertion failed", without showing the values.
+   *
+   * Try it out! Change the values so that the assertion fails, and look at the
+   * error message.
+   */
+  // test("adding ints") {
+  //   assert(1 + 2 === 3)
+  // }
+
+
+  import FunSets._
+
+  test("contains is implemented") {
+    assert(contains(x => true, 100))
+  }
+
+  /**
+   * When writing tests, one would often like to re-use certain values for multiple
+   * tests. For instance, we would like to create an Int-set and have multiple test
+   * about it.
+   *
+   * Instead of copy-pasting the code for creating the set into every test, we can
+   * store it in the test class using a val:
+   *
+   *   val s1 = singletonSet(1)
+   *
+   * However, what happens if the method "singletonSet" has a bug and crashes? Then
+   * the test methods are not even executed, because creating an instance of the
+   * test class fails!
+   *
+   * Therefore, we put the shared values into a separate trait (traits are like
+   * abstract classes), and create an instance inside each test method.
+   *
+   */
+
+  trait TestSets {
+    val s1 = singletonSet(1)
+    val s2 = singletonSet(2)
+    val s3 = singletonSet(3)
+  }
+
+  /**
+   * This test is currently disabled (by using "ignore") because the method
+   * "singletonSet" is not yet implemented and the test would fail.
+   *
+   * Once you finish your implementation of "singletonSet", exchange the
+   * function "ignore" by "test".
+   */
+  test("singletonSet(1) contains 1") {
+
+    /**
+     * We create a new instance of the "TestSets" trait, this gives us access
+     * to the values "s1" to "s3".
+     */
+    new TestSets {
+      /**
+       * The string argument of "assert" is a message that is printed in case
+       * the test fails. This helps identifying which assertion failed.
+       */
+      assert(contains(s1, 1), "Singleton")
+    }
+  }
+
+  private val isEven: (Int) => Boolean = i => i % 2 == 0
+  private val divisibleBy4: Set = i => i % 4 == 0
+  private val divisibleBy3: Set = i => i % 3 == 0
+
+  test("for all for sets") {
+    assert(forall(divisibleBy4, isEven), "even numbers")
+    assert(!forall(divisibleBy3, isEven), "even numbers")
+  }
+
+  test("filter") {
+    assert(contains(filter(divisibleBy4, isEven),4))
+    assert(!contains(filter(divisibleBy4, isEven),2))
+  }
+
+  test("exists") {
+    assert(exists(isEven, i => i == 100), "100 exists")
+    assert(!exists(isEven, i => i == 10000), "10000 exists")
+  }
+
+  test("map") {
+    val evenOver100: Set = map(i => i > 100, i => 2 * i)
+    printSet(evenOver100)
+
+    assert(!contains(evenOver100, 98))
+    assert(!contains(evenOver100, 102))
+    assert(!contains(evenOver100, 202))
+
+    assert(forall(evenOver100, i => i%2 == 0), "all are even")
+  }
+
+  test("union contains all elements of each set") {
+    new TestSets {
+      val s = union(s1, s2)
+      assert(contains(s, 1), "Union 1")
+      assert(contains(s, 2), "Union 2")
+      assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+
+}
