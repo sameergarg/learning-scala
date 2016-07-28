aca85a97420ef9e00d9d7642340b3353cdcae490
sameergarg <garg.sameer@gmail.com>
2016-07-28 22:56:28 +0100

    Parallel Programming : Programming Assignment: Reductions and Prefix Sums     Balance parantheses

--- specialization/parallelprogramming/week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ specialization/parallelprogramming/week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -40,23 +40,81 @@ object ParallelParenthesesBalancing {
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
+  //using double accumulator
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    usingDoubleAcc(chars)
+  }
+
+  //using single
+  def balance2(chars: Array[Char]): Boolean = {
+    usingSingleAcc(chars)
+  }
+
+  private def usingDoubleAcc(chars: Array[Char]): Boolean = {
+    def loop(openParenAcc: Int, closeParenAcc: Int, remChars: List[Char]): Boolean = remChars match {
+      case _ if (openParenAcc - closeParenAcc) < 0 => false
+      case Nil => openParenAcc - closeParenAcc == 0
+      case h :: tail if (h == '(') => loop(openParenAcc + 1, closeParenAcc, tail)
+      case h :: tail if (h == ')') => loop(openParenAcc, closeParenAcc + 1, tail)
+      case h :: tail => loop(openParenAcc, closeParenAcc, tail)
+    }
+
+    loop(0, 0, chars.toList)
+  }
+
+  private def usingSingleAcc(chars: Array[Char]): Boolean = {
+    def loop(acc: Int, remChars: List[Char]): Int = remChars match {
+      case Nil => acc
+      case _ if (acc < 0) => acc
+      case head :: tail if (head == '(') => loop(acc + 1, tail)
+      case head :: tail if (head == ')') => loop(acc - 1, tail)
+      case head :: tail => loop(acc, tail)
+
+    }
+
+    loop(0, chars.toList) == 0
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, openParen: Int, closeParen: Int) : (Int, Int) =
+      if(idx < until){
+        chars(idx) match {
+          case '(' => traverse(idx+1, until, openParen+1, closeParen)
+          case ')' =>
+            if(openParen > 0)
+              traverse(idx+1, until, openParen - 1, closeParen) //close paren belongs to same part of characters
+            else
+              traverse(idx+1, until, openParen, closeParen+1) // close paren matches some other open paren outside the current chars
+          case _ => traverse(idx+1, until, openParen, closeParen)
+        }
+      }else
+        (openParen, closeParen)
+
+    def reduce(from: Int, until: Int) :  (Int, Int) = {
+      val size = until-from
+
+      if(size <= threshold) {
+        traverse(from, until, 0, 0)
+      } else {
+        val mid = from + size/2
+        val ((openParen1,closeParen1), (openParen2, closeParen2)) =
+          parallel(
+           reduce(from, mid),
+           reduce(mid, until)
+        )
+
+        if(openParen1 > 0)
+          (openParen1 - closeParen2 + openParen2, closeParen1)
+        else
+          (openParen1+openParen2, closeParen1+closeParen2)
+
+      }
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
-    }
-
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
