4cb5e964433c64a8c32f467f999ab6908fbc2ffd
sameer garg <garg.sameer@gmail.com>
2018-06-13 14:19:57 +0100

    identity spec added

---
+++ learning-cats/src/test/scala/scala/excersice/cats/IdentitySpec.scala
@@ -0,0 +1,31 @@
+package scala.excersice.cats
+
+import cats.{Applicative, Functor, Id, Monad}
+import cats.implicits._
+import org.scalatest.{Matchers, WordSpec}
+
+class IdentitySpec extends WordSpec with Matchers {
+
+  "Id" should {
+    "int id" in {
+      val anId: Id[Int] = 42
+      anId should be(42)
+    }
+
+    "map" in {
+      val one: Int = 1
+      Functor[Id].map(one)(_ + 1) should be(2)
+    }
+
+    "pure" in {
+      Applicative[Id].pure(42) should be(42)
+    }
+
+    "map and flatmap" in {
+      val one: Int = 1
+      Monad[Id].map(one)(_ + 1)
+      Monad[Id].flatMap(one)(_ + 1)
+    }
+  }
+
+}
